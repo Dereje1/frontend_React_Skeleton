157d0043cdf57395990228e753d8f41d6211d13d
Dereje1 <Dereje1>
2018-01-22 14:52:16 -0500

    modified skeleton

---
+++ src/child.js
@@ -0,0 +1,14 @@
+"use strict"
+import React, { Component } from 'react';
+
+class Child extends Component {
+
+  render() {
+    return (
+      <h2 className="text-center">Hello World!</h2>
+    );
+  }
+
+}
+
+export default Child;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
+"use strict" //Master Component
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 
+import Child from './child'
 class Home extends Component {
   render() {
     return (
-      <h1 className="text-center">Hello World!!</h1>
+      <div>
+        <h1 className="text-center">Hello World!!</h1>
+        <Child />
+      </div>
     );
   }
 }
